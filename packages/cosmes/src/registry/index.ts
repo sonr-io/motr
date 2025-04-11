ac26ccc0a961a8ec9350cac59d47b3627f077ae5
Prad N <prad@sonr.io>
2025-04-11 15:12:32 -0400

    feat/refactor-motr

---
+++ packages/cosmes/src/registry/index.ts
@@ -0,0 +1,6 @@
+export type * from "@keplr-wallet/types";
+export type { ChainRegistryAssetList } from "./types/ChainRegistryAssetList";
+export type { ChainRegistryChainInfo } from "./types/ChainRegistryChainInfo";
+
+export { getChainRegistryAssetList } from "./apis/getChainRegistryAssetList";
+export { getChainRegistryChainInfo } from "./apis/getChainRegistryChainInfo";
