1429a8a09f11fbff5261ef81b1f53e885d7a1ffe
Prad N <prad@sonr.io>
2025-04-18 13:39:26 -0400

    <no value>

--- cmd/vault/worker.mjs
+++ cmd/vault/worker.mjs
@@ -1,7 +1,7 @@
 export { default } from './build/worker.mjs';
 import createPlugin from '@extism/extism';
 
-export class Controller {
+export class Vault {
   constructor(state, env) {
     this.state = state;
   }
