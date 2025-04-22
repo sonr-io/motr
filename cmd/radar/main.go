d362df39adfc05442e3c59b9087318a88a3b58d4
Prad N <prad@sonr.io>
2025-04-21 23:56:23 -0400

    feat: enhance radar with session and cache middleware

---
+++ cmd/radar/main.go
@@ -0,0 +1,27 @@
+//go:build js && wasm
+// +build js,wasm
+
+package main
+
+import (
+	"github.com/sonr-io/motr/config"
+	"github.com/sonr-io/motr/internal/middleware"
+	"github.com/sonr-io/motr/x/auth"
+	"github.com/sonr-io/motr/x/landing"
+)
+
+func main() {
+	// Setup config
+	e, c := config.New()
+	e.Use(middleware.UseSession(c), middleware.UseCloudflareCache(c))
+
+	// Register controllers
+	if err := landing.Register(c, e); err != nil {
+		panic(err)
+	}
+	if err := auth.Register(c, e); err != nil {
+		panic(err)
+	}
+	// Start server
+	e.Serve()
+}
