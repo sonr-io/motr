8c5ea8bd2b8487252e9bba0ffe52bcf0b7ebb092
Prad Nukala <prad@sonr.io>
2025-11-06 16:28:46 -0500

    chore(): remove @sonr.io/browser package

--- pkgs/browser/src/web-apis/index.ts
+++
@@ -1,9 +0,0 @@
-/**
- * Web APIs exports
- *
- * Re-exports all Web Standards-based APIs including custom elements,
- * events, and observers.
- */
-
-// Elements
-export * from "./elements/index.js";
--- pkgs/react/src/providers/index.ts
+++
@@ -1,5 +0,0 @@
-/**
- * Context providers for @sonr.io/react
- */
-
-export { EnclaveProvider, useEnclaveContext } from "./EnclaveProvider";
