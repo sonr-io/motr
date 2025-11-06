30514d0341bdf18c048f7cfef3fbdfc5ca63d708
Prad Nukala <prad@sonr.io>
2025-11-06 16:28:15 -0500

    chore(profile): Delete React UI

--- apps/profile/src/main.tsx
+++
@@ -1,8 +0,0 @@
-import '@sonr.io/ui/styles/globals.css';
-import { renderAppRouter } from '@sonr.io/ui';
-import { routeTree } from './routeTree.gen';
-
-renderAppRouter({
-  routeTree,
-  showDevtools: import.meta.env.DEV,
-});
