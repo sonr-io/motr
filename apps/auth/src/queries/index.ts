b396927106bc030f9d28ac0933aed7d004130ab4
Prad Nukala <prad@sonr.io>
2025-11-18 11:43:12 -0500

    remove: auth app

--- apps/auth/src/queries/index.ts
+++
@@ -1,3 +0,0 @@
-export * from "./useAccountQuery";
-export * from "./useBalanceQuery";
-export * from "./useTxQuery";
--- apps/auth/src/workflows/index.ts
+++
@@ -1,8 +0,0 @@
-/**
- * Cloudflare Workflows for Frontend
- *
- * Export all workflow entrypoints for the Sonr frontend application
- */
-
-export { OTPEmailWorkflow } from './otp-email';
-export type { OTPEmailParams, OTPEmailResult, Env as OTPEmailEnv } from './otp-email';
